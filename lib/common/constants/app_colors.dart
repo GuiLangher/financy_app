76b9999b75a24ec49df7eceb37f3ed9fa591d0b6
legacysistemas <guilherme.langher@gmail.com>
2025-03-12 21:56:01 -0300

    splashScreen

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._(); //Acessa o construtor
+
+  static const Color greenlightOne = Color(0xFF63B5AF);
+  static const Color greenlightTwo = Color(0xFF438883);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
