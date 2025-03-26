213ee371ea01f26b1d84b4fc489c86256f9bc6e3
legacysistemas <guilherme.langher@gmail.com>
2025-03-26 20:01:15 -0300

    button_get_started

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -15,14 +15,58 @@ class OnBoardingPage extends StatelessWidget {
             child: Container(
               color: AppColors.iceWhite,
               child: Image.asset('assets/images/main_image.png'),
-            ), 
+            ),
           ),
-           Text('Spend Smarter', style: AppTextStyles.mediumText.copyWith(color: AppColors.greenlightTwo)),
-           Text('Save More', style: AppTextStyles.mediumText.copyWith(color: AppColors.greenlightTwo)),
-           ElevatedButton(onPressed: (){}, child: Text('Começar')),
-           Text('Já tem conta? Log In', style: AppTextStyles.smallText.copyWith(color: AppColors.grey)),
-           SizedBox(height: 60.0,)
-      ],),
+          Text(
+            'Spend Smarter',
+            style: AppTextStyles.mediumText.copyWith(
+              color: AppColors.greenlightTwo,
+            ),
+          ),
+          Text(
+            'Save More',
+            style: AppTextStyles.mediumText.copyWith(
+              color: AppColors.greenlightTwo,
+            ),
+          ),
+          SizedBox(height: 16.0),
+          Material(
+            borderRadius: BorderRadius.all(Radius.circular(38.0)),
+            child: Ink(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(38.0)),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [AppColors.greenlightOne, AppColors.greenlightTwo],
+                ),
+              ),
+              child: InkWell(
+                borderRadius: const BorderRadius.all(Radius.circular(38.0)),
+                onTap: () {},
+                child: Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(38.0))
+                  ),
+                  alignment: Alignment.center,
+                  height: 64.0,
+                  child: Text(
+                    'Get Started',
+                    style: AppTextStyles.mediumText18.copyWith(
+                      color: AppColors.whiteColor,
+                    ),
+                    ),
+                ),
+              ),
+            ),
+          ),
+          Text(
+            'Já tem conta? Log In',
+            style: AppTextStyles.smallText.copyWith(color: AppColors.grey),
+          ),
+          SizedBox(height: 60.0),
+        ],
+      ),
     );
   }
 }
